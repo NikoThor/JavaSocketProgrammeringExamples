9285dcb346a3556e0df3f1088c9b3186b623ede6
Nikolaj Thor Christensen <nikolajdk1@gmail.com>
2020-08-24 13:06:24 +0200

    Finished DateServer class, server now running

--- src/DateServer/DateServer.java
+++ src/DateServer/DateServer.java
@@ -1,15 +1,18 @@
 package DateServer;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.util.Date;
 
 public class DateServer {
     public static void main(String[] args) throws IOException {
         try(var listener = new ServerSocket(8080)) {
             System.out.println("The Date Server is running....");
 
-            while(true) {
-
+            try (var socket = listener.accept()) {
+                var out = new PrintWriter(socket.getOutputStream(), true);
+                out.println(new Date().toString());
             }
         }
 
